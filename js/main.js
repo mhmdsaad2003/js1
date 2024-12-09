b172ba07a5a8b74e8ce13ec1ed1516572b6a52f5
Mohamed saad <153324186+mhmdsaad2003@users.noreply.github.com>
2024-12-09 19:58:38 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("moo").innerHTML="hellow from moo";
+
+document.getElementById("moo").style.color="blue";
+
+document.getElementById("moo").style.textAlign="center";
+
+//("mohamed");
+
+console.log("mohamed saad")
+
+//document.write("ahamed")
